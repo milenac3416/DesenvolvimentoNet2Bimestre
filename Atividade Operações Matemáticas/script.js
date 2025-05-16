eaf9b34f5726c4d16cc5b25060a38c81abf2ebec
milena <milena200100@edu.unipar.br>
2025-05-15 22:40:20 -0300

    Update script.js

--- Atividade Operações Matemáticas/script.js
+++ Atividade Operações Matemáticas/script.js
@@ -1,14 +1,28 @@
 let num1 = document.querySelector("#num1");
 let num2 = document.querySelector("#num2");
 let botao = document.querySelector("#botao");
-let resultado = document.querySelector("#resultado");
+let resultado1 = document.querySelector("#resultado1");
+let resultado2 = document.querySelector("#resultado2");
+let resultado3 = document.querySelector("#resultado3");
+let resultado4 = document.querySelector("#resultado4");
  
-function (){
-    let soma = num1 + num2;
-    let sub = num1 - num2;
-    let mult = num1 * num2;
-    let div = num1 / num2;
+function calcular (){
+    let n1 = Number(num1.value);
+    let n2 = Number(num2.value);
 
-    resultado.textContent = 
+    let soma = n1 + n2;
+    let sub = n1 - n2;
+    let mult = n1 * n2;
+    let div = n1 / n2;
+
+    resultado1.textContent ='Soma' + soma;
+    resultado2.innerHTML = 'Subtra&ccedil&atildeo' +sub;
+    resultado3.textContent = mult;
+    resultado4.textContent = div;
 
 }
+botao.onclick = function (){
+    calcular ();
+}
+
+
