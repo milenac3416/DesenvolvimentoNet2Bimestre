8d5840087f7786a6c09c1a8e774d2572fabf5f90
Milena Carvalho <milena200100@edu.unipar.br>
2025-05-18 20:38:23 -0300

    Add files via upload

---
+++ Sabores Pizza/script.js
@@ -0,0 +1,21 @@
+let sabor1 = document.querySelector("#sabor1");
+let sabor2 = document.querySelector("#sabor2");
+let sabor3 = document.querySelector("#sabor3");
+let sabor4 = document.querySelector("#sabor4");
+let refrigerantes = document.querySelector("#refrigerantes");
+let botao = document.querySelector("#botao");
+let total = document.querySelector("#total");
+
+botao.onclick = function () {
+  let refri = Number(refrigerantes.value);
+  let valorTotal = 4 * 12 + refri * 7;
+
+  total.innerHTML =
+    "Sabores escolhidos: " +
+    sabor1.value + ", " +
+    sabor2.value + ", " +
+    sabor3.value + ", " +
+    sabor4.value + "<br>" +
+    "Refrigerantes: " + refri + "<br>" +
+    "Total: R$ " + valorTotal;
+}
